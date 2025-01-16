be4359cd57b40e2012f04bb6422b222e7049529a
leasorssini <dlegasr@gmail.com>
2025-01-16 03:13:40 -0300

    teste2

--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -1,2 +1,6 @@
 # Região da AWS
 aws_region = "us-east-1"
+cognito_user_pool_id  = "your-cognito-user-pool-id"
+cognito_client_id     = "your-cognito-client-id"
+dynamo_db_user        = "your-dynamo-db-user"
+dynamo_db_password    = "your-dynamo-db-password"
--- infra/variables.tf
+++ infra/variables.tf
@@ -4,3 +4,23 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "cognito_user_pool_id" {
+  description = "Cognito User Pool ID"
+  type        = string
+}
+
+variable "cognito_client_id" {
+  description = "Cognito App Client ID"
+  type        = string
+}
+
+variable "dynamo_db_user" {
+  description = "DynamoDB Database User"
+  type        = string
+}
+
+variable "dynamo_db_password" {
+  description = "DynamoDB Database Password"
+  type        = string
+}
