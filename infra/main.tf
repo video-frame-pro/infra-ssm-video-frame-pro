7b157e4aa495b0584db4a901d91731d06b5b211f
leasorssini <dlegasr@gmail.com>
2025-01-16 13:01:15 -0300

    life cycle

--- infra/main.tf
+++ infra/main.tf
@@ -2,54 +2,54 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Verifica se o parâmetro já existe
-data "aws_ssm_parameter" "cognito_user_pool_id" {
-  name = "/cognito/user_pool_id"
-}
-
-# Cria o parâmetro apenas se ele não existir
+# Parâmetro Cognito User Pool ID
 resource "aws_ssm_parameter" "cognito_user_pool_id" {
-  count       = length(data.aws_ssm_parameter.cognito_user_pool_id.*.name) == 0 ? 1 : 0
   name        = "/cognito/user_pool_id"
   type        = "SecureString"
   value       = var.cognito_user_pool_id
   description = "Cognito User Pool ID"
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [value]  # Não altera o valor existente
+  }
 }
 
-# Repetir o mesmo padrão para os outros parâmetros
-
-data "aws_ssm_parameter" "cognito_client_id" {
-  name = "/cognito/client_id"
-}
-
+# Parâmetro Cognito App Client ID
 resource "aws_ssm_parameter" "cognito_client_id" {
-  count       = length(data.aws_ssm_parameter.cognito_client_id.*.name) == 0 ? 1 : 0
   name        = "/cognito/client_id"
   type        = "SecureString"
   value       = var.cognito_client_id
   description = "Cognito App Client ID"
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [value]  # Não altera o valor existente
+  }
 }
 
-data "aws_ssm_parameter" "dynamo_db_user" {
-  name = "/dynamo/db_user"
-}
-
+# Parâmetro DynamoDB Database User
 resource "aws_ssm_parameter" "dynamo_db_user" {
-  count       = length(data.aws_ssm_parameter.dynamo_db_user.*.name) == 0 ? 1 : 0
   name        = "/dynamo/db_user"
   type        = "SecureString"
   value       = var.dynamo_db_user
   description = "DynamoDB Database User"
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [value]  # Não altera o valor existente
+  }
 }
 
-data "aws_ssm_parameter" "dynamo_db_password" {
-  name = "/dynamo/db_password"
-}
-
-resource "aws_ssm_parameter" "dynamo_db_password" {
-  count       = length(data.aws_ssm_parameter.dynamo_db_password.*.name) == 0 ? 1 : 0
-  name        = "/dynamo/db_password"
-  type        = "SecureString"
-  value       = var.dynamo_db_password
-  description = "DynamoDB Database Password"
-}
+# Parâmetro DynamoDB Database Password
+# resource "aws_ssm_parameter" "dynamo_db_password" {
+#   name        = "/dynamo/db_password"
+#   type        = "SecureString"
+#   value       = var.dynamo_db_password
+#   description = "DynamoDB Database Password"
+#
+#   lifecycle {
+#     create_before_destroy = true
+#     ignore_changes        = [value]  # Não altera o valor existente
+#   }
+# }
